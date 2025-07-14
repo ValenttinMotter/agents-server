32b1bff4c6a2fa622e3e757e0bf7bf8db5285aa4
Valenttin Motter <valenttin@myscrumhalf.com>
2025-07-13 22:25:00 -0300

    feat: finishing backend

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,14 @@ import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 import { createRoomRoute } from "./http/routes/create-room.ts";
 import { getRoomQuestionsRoute } from "./http/routes/get-room-questions.ts";
 import { createQuestionRoute } from "./http/routes/create-question.ts";
+import { uploadAudioRoute } from "./http/routes/upload-audio.ts";
+import fastifyMultipart from "@fastify/multipart";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, { origin: "http://localhost:5173" });
+
+app.register(fastifyMultipart);
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -26,5 +30,6 @@ app.register(getRoomsRoute);
 app.register(createRoomRoute);
 app.register(getRoomQuestionsRoute);
 app.register(createQuestionRoute);
+app.register(uploadAudioRoute);
 
 app.listen({ port: env.PORT });
